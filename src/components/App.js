0a8df6ef476cd7479e1e6db774580c737cc21d83
Michael Campos <camposmj89@gmail.com>
2018-03-24 16:26:43 -0700

    Resolved merge conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import LambdaDemo from './lambdaDemo';
+import { InstantSearch, Hits, Highlight, SearchBox } from 'react-instantsearch/dom';
 
 function Product({ hit }) {
   return (
     <div style={{ marginTop: '10px' }}>
       <span className="hit-name">
         <Highlight attribute="name" hit={hit} />
-        <div>{hit.local_date}</div>
       </span>
     </div>
   );
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,52 +1,12 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
-import App from './App'
-import TimeView from './TimeView'
-import MapView from './MapView'
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic}/>
-    <Route exact path={match.path} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
+} from 'react-router-dom';
+import App from './App';
+import TimeView from './TimeView';
+import MapView from './MapView';
 
 const AppRouter = () => (
   <Router>
@@ -54,15 +14,17 @@ const AppRouter = () => (
       <ul>
         <li><Link to="/">List</Link></li>
         <li><Link to="/map">Map</Link></li>
-        <li><Link to="/timeline">Timeline</Link></li>
+        <li>
+        <Link to="/timeline">Timeline</Link></li>
       </ul>
 
       <hr/>
 
-      <Route exact path="/" component={App}/>
-      <Route path="/map" component={MapView}/>
+      <Route exact path="/" component={App} />
+      <Route path="/map" component={MapView} />
       <Route path="/timeline" component={TimeView}/>
     </div>
   </Router>
-)
-export default AppRouter
+);
+
+export default AppRouter;
