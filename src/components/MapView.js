f929979bb4febf425ec8b8b4f664d46bc9586086
Neil Ricci <iccir919@gmail.com>
2018-03-24 15:53:05 -0700

    Add map with react-google-maps

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 import './MapView.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+    />
+  </GoogleMap>
+));
+
 
 class MapView extends Component {
   constructor(props) {
@@ -15,9 +33,12 @@ class MapView extends Component {
   render() {
     const {loading, msg} = this.state;
 
-    return <p>
-      Map
-    </p>
+    return <MapWithAMarker
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
   }
 }
 
