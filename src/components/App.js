dd61ba3c3bb58716c6bee1015cea0d649741a084
Vincent Song <vincent@MacBook-Pro-2.local>
2018-03-24 18:51:55 -0700

    css style changes

--- src/components/App.js
+++ src/components/App.js
@@ -27,32 +27,34 @@ class LambdaDemo extends Component {
   }
 }
 
+// style={{ marginTop: '10px' }}
 function HitItem({ hit }) {
   console.log(hit);
   return (
-    <div className='hit-item' style={{ marginTop: '10px' }}>
-      <span className="hit-name">
-        <Highlight attribute="name" hit={hit} />
-        {(hit.percent_full) ? <div>Capacity {hit.percent_full}%</div>: <div>No Capacity</div> }
-        <div>Going {hit.yes_rsvp_count}</div>
-        <div>{hit.local_date} {hit.local_time}</div>
-        <a href={hit.link} target="_blank">{hit.link}</a>
-
-      </span>
+    <div className='hit-item'>
+      <Highlight attribute="name" hit={hit} />
+      <div className='hit-name-sub'>
+        {(hit.percent_full) ? <span className="hit-name-sub-capacity">Capacity {hit.percent_full}% </span> : <span className="hit-name-sub-capacity">Capacity</span> }
+        <span className="hit-name-sub-attendance">Going {hit.yes_rsvp_count} </span>
+        <span className="hit-name-sub-date">{hit.local_date} {hit.local_time} </span>
+      </div>
     </div>
   );
 }
+// <a href={hit.link} target="_blank">{hit.link}</a>
+
+// <header className="App-header">
+//   <h1 className="App-title">Welcome to React</h1>
+// </header>
+// <p className="App-intro">
+//   To get started, edit <code>src/App.js</code> and save to reload.
+// </p>
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <InstantSearch
           appId="8AOQAW9EZA"
           apiKey="8caaee2531c0957918fc460b01e2acbb"
