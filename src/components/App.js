59fa58c1d661f647275814ba2a8a84e7579a5c2c
Tiffany Lam <tlam5@sbcglobal.net>
2018-03-25 13:35:54 -0700

    Remove Router and update header

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">SF Hot Events</h1>
         </header>
 
         <InstantSearch
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Router from './components/Router';
+import App from './components/App';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
