4760fdbc388a09df340bfcc83f1aa36159b23efe
Neil Ricci <iccir919@gmail.com>
2018-03-24 17:38:04 -0700

    Insert markers of events

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -6,15 +6,18 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import data from "../data/latsAndLons.json";
 
 const MapWithAMarker = withScriptjs(withGoogleMap(props =>
   <GoogleMap
     defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
   >
-    <Marker
-      position={{ lat: -34.397, lng: 150.644 }}
-    />
+  {
+    data.map(function(event, i){
+      return <Marker key={i} position={{ lat: event.lat, lng: event.lon }} />
+    })
+  }
   </GoogleMap>
 ));
 
@@ -32,13 +35,12 @@ class MapView extends Component {
 
   render() {
     const {loading, msg} = this.state;
-
     return <MapWithAMarker
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+      mapElement={<div style={{ height: `100%` }} />}>
+      </MapWithAMarker>
   }
 }
 
