6d98da753b8a422e3c8cc996b4a7d347e0246a77
Michael Campos <camposmj89@gmail.com>
2018-03-25 15:50:54 -0700

    Imported momentJS to format time and show how much time has passed since event or how much time until an event begins.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import { InstantSearch, Hits, Highlight, SearchBox } from 'react-instantsearch/dom';
-import Product from './Product';
 import MapContainer from './MapContainer';
+import moment from 'moment';
+
+function timeDifference(date, time) {
+	return moment(date).format('MMMM Do, YYYY') + " @ " + moment(time).format("h:mma") + ` (${moment(time).fromNow()})`;
+}
 
 function HitItem({ hit }) {
-  return (
-    <div className='hit-item'>
-      <Highlight className='title' attribute="name" hit={hit} />
-      <div className='hit-name-sub'>
-        {(hit.percent_full) ? <span className="hit-name-sub-capacity">Capacity {hit.percent_full}% </span> : <span className="hit-name-sub-capacity">Capacity</span> }
-        <span className="hit-name-sub-attendance">Going {hit.yes_rsvp_count} </span>
-        <span className="hit-name-sub-date">{hit.local_date} {hit.local_time} </span>
-      </div>
-    </div>
-  );
+	return (
+		<div className='hit-item'>
+		  <Highlight className='title' attribute="name" hit={hit} />
+
+			<div className='hit-name-sub'>
+				{(hit.percent_full) ? <span className="hit-name-sub-capacity">Capacity {hit.percent_full}% </span> : <span className="hit-name-sub-capacity">Capacity</span> }
+				<span className="hit-name-sub-attendance">Going {hit.yes_rsvp_count} </span>
+				<span className="hit-name-sub-date">{timeDifference(hit.local_date, hit.time)} </span>
+			</div>
+		</div>
+	);
 }
 
 class App extends Component {
