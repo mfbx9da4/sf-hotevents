dfcf55206909370f5558ef1dc4cd2ea60c908b9e
Neil Ricci <iccir919@gmail.com>
2018-03-25 14:51:35 -0700

    Bring the data back to list

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import { InstantSearch, Hits, Highlight, SearchBox } from 'react-instantsearch/dom';
+import Product from './Product';
 import MapContainer from './MapContainer';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
+
+function HitItem({ hit }) {
+  return (
+    <div className='hit-item'>
+      <Highlight className='title' attribute="name" hit={hit} />
+      <div className='hit-name-sub'>
+        {(hit.percent_full) ? <span className="hit-name-sub-capacity">Capacity {hit.percent_full}% </span> : <span className="hit-name-sub-capacity">Capacity</span> }
+        <span className="hit-name-sub-attendance">Going {hit.yes_rsvp_count} </span>
+        <span className="hit-name-sub-date">{hit.local_date} {hit.local_time} </span>
+      </div>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
-	function HitItem(hit) {
-		return (
-			<div className='hit-item'>
-				<Highlight className='title' attribute="name" hit={hit} />
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">SF Hot Events</h1>
+        </header>
 
-				<div className='hit-name-sub'>
-					{(hit.percent_full) ? <span className="hit-name-sub-capacity">Capacity {hit.percent_full}% </span> : <span className="hit-name-sub-capacity">Capacity</span> }
-					<span className="hit-name-sub-attendance">Going {hit.yes_rsvp_count} </span>
-					<span className="hit-name-sub-date">{hit.local_date} {hit.local_time} </span>
-				</div>
-			</div>
-		);
-	}
-
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1 className="App-title">SF Hot Events</h1>
-			</header>
-
-			<InstantSearch
-			appId="8AOQAW9EZA"
-			apiKey="8caaee2531c0957918fc460b01e2acbb"
-			indexName="sf-events"
-			>
-				<SearchBox/>
+        <InstantSearch
+          appId="8AOQAW9EZA"
+          apiKey="8caaee2531c0957918fc460b01e2acbb"
+          indexName="sf-events"
+        >
+          <SearchBox />
           <div className="container">
             <MapContainer></MapContainer>
             <Hits hitComponent={HitItem} className="events-list"/>
